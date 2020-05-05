152b26f1897e54dc42cbb4f0369c49b4741e7d03
adnanioricce <adnanioricce@outlook.com>
2020-05-05 13:39:44 -0300

    creating a mediator between services from legacy system and current. Also adding some code for updating to implement later

--- src/Presentation/Desktop/ViewModels/Product/CreateProductViewModel.cs
+++ src/Presentation/Desktop/ViewModels/Product/CreateProductViewModel.cs
@@ -1,9 +1,23 @@
-﻿using GalaSoft.MvvmLight;
+﻿using Core.Entities.Catalog;
+using Core.Interfaces;
+using Core.Interfaces.Catalog;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace Desktop.ViewModels.Product
 {
     public class CreateProductViewModel : ViewModelBase
-    {
-        
+    {        
+        private readonly IDrugProdutoMediator _drugProdutoMediator;
+        public RelayCommand<Drug> CreateDrugCommand { get; set; }
+        public CreateProductViewModel(IDrugProdutoMediator drugProdutoMediator)
+        {            
+            _drugProdutoMediator = drugProdutoMediator;
+            CreateDrugCommand = new RelayCommand<Drug>(_drugProdutoMediator.CreateDrugFrom,CanExecuteCreateDrug);
+        }        
+        public bool CanExecuteCreateDrug(Drug drug)
+        {
+            return true;
+        }
     }
 }
