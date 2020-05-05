2d1f338c07ceee17437e8a4334828e5da5fdc30a
adnanioricce <adnanioricce@outlook.com>
2020-05-05 13:43:48 -0300

    deleting edit_manifest.ps1, removing some lines of the pwsh script on azure-pipelines and adding a line for msix files on gitignore

--- edit_manifest.ps1
+++
@@ -1,7 +0,0 @@
-[xml]$manifestFile = Get-Content src/Presentation/Desktop/AppxManifest.xml
-$manifestFile.Package.Applications.Application.Executable = "Desktop.msix"
-echo $manifestFile.Package.Applications.Application.Executable
-$manifestFile.Save("./temp.xml")
-#foreach($node in $manifestFile.Applications.FirstChild){
-#	Write-Host $node.Attributes
-#}
