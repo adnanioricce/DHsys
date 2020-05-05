13c5b984e64c7764574008bf438cfc309d120952
adnanioricce <adnanioricce@outlook.com>
2020-05-05 13:39:44 -0300

    creating a mediator between services from legacy system and current. Also adding some code for updating to implement later
    Former-commit-id: 8183aa54ae7930ea67da3b863d77bdaf28e15841

--- src/Libraries/Infrastructure/Updates/Updater.cs
+++ src/Libraries/Infrastructure/Updates/Updater.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using Infrastructure.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using NetSparkle;
+using Microsoft.Extensions.Options;
+using Infrastructure.Settings;
+using NetSparkle.Enums;
+using System.Reflection;
 
 namespace Infrastructure.Updates
 {
-    class Updater
+    public class Updater
     {
+        private readonly IAppLogger<Updater> _logger;
+        private readonly AutoUpdateSettings _settings;
+        
+        public Updater(IAppLogger<Updater> logger,
+            IOptions<AutoUpdateSettings> settings)
+        {
+            _logger = logger;
+            _settings = settings.Value;
+        }
+        public void ConfigureUpdater()
+        {
+            var sparkle = new Sparkle(_settings.AppCastUrl,
+                new System.Drawing.Icon(_settings.AppIcon,32,32),
+                securityMode:ToSecurityMode(_settings.SecurityMode),
+                dsaPublicKey:_settings.DsaPublicKey,
+                Assembly.GetExecutingAssembly().GetName().FullName);
+            //TODO:
+            
+        }
+        public bool HasUpdate() 
+        {
+            //TODO:
+            return true;
+        }
+        public async Task Update(bool silently)
+        {
+            //TODO:            
+        }
+        private SecurityMode ToSecurityMode(string securityMode)
+        {
+            switch (securityMode)
+            {
+                case "Strict":
+                    return SecurityMode.Strict;
+                case "Unsafe":
+                    return SecurityMode.Unsafe;
+                case "UseIfPossible":
+                    return SecurityMode.UseIfPossible;
+                default:
+                    return SecurityMode.Strict;
+            }
+        }
     }
 }
