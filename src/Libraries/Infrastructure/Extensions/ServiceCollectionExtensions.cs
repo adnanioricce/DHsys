72af88f95265cc606366fb3cedbae9c130abe93d
adnanioricce <adnanioricce@outlook.com>
2020-05-09 04:34:20 -0300

    adding code for automatic application update

--- src/Libraries/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Libraries/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using System;
+using System.IO;
+
 namespace Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
@@ -18,6 +21,51 @@ namespace Infrastructure.Extensions
                 var options = provider.GetService<IOptionsMonitor<T>>();
                 return new OptionWriter<T>(environment, file);
             });
+        }
+        public static void ConfigureAppDataFolder(this IServiceCollection services,IConfiguration configuration,string myFolder = "DHsys")
+        {
+            //TODO:Use isolation storage instead
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),myFolder);
+            if (!Directory.Exists(folder))
+            {
+                try{
+                    Directory.CreateDirectory(folder);
+                }catch(Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            //Create appsettings.json file on AppData Folder
+            string appsettingsPath = Path.Combine(folder,"appsettings.json");
+            if (!File.Exists(appsettingsPath))
+            {
+                if (File.Exists("appsettings.json"))
+                {
+                    var appsettingsContent = File.ReadAllLines("appsettings.json");                    
+                    var lines = string.Join("\n", appsettingsContent);
+                    File.WriteAllText(appsettingsPath, lines);
+                    return;
+                }
+                File.Create(appsettingsPath);
+            }
+            //create updates folder on AppData Folder
+            string updateFilesFolder = Path.Combine(folder,"Updates");
+            if (!File.Exists(updateFilesFolder))
+            {
+                Directory.CreateDirectory(updateFilesFolder);
+            }
+        }
+        public static void SetUpdateSettings(this IServiceCollection services)
+        {
+            using (var provider = services.BuildServiceProvider())
+            {
+                var updateSettingsWriter = provider.GetService<OptionWriter<AutoUpdateSettings>>();
+                updateSettingsWriter.Update(settings =>
+                {
+                    settings.UpdateFileUrl
+                   //TODO:Add dsa public key path
+                });
+            }
         }
     }
 }
--- src/Libraries/Infrastructure/Settings/AutoUpdateSettings.cs
+++ src/Libraries/Infrastructure/Settings/AutoUpdateSettings.cs
@@ -2,10 +2,11 @@
 {
     public class AutoUpdateSettings
     {
-        public string AppCastUrl { get; set; }
+        public string UpdateFileUrl { get; set; }
         public string DsaPublicKey { get; set; }
         public string ReferenceAssembly { get; set; }
         public string SecurityMode { get; set; }
         public string AppIcon { get; set; }
+        public bool ShouldUpdateSilently { get; set; }
     }
 }
