0c281fa92734b64d1e473872a299232506634ee0
adnanioricce <adnanioricce@outlook.com>
2020-06-09 22:01:38 +0000

    adding first test of future transaction service

--- src/Libraries/Core/Entities/Financial/Transaction.cs
+++ src/Libraries/Core/Entities/Financial/Transaction.cs
@@ -5,6 +5,8 @@ namespace Core.Entities.Financial
     public class Transaction : BaseEntity
     {                
         public decimal TransactionTotal { get; set; }
-        public virtual ICollection<TransactionItem> Items { get; set; }        
+        public virtual PaymentMethods PaymentMethod { get; set; }
+        public bool HasDealWithStore { get; set; }        
+        public virtual ICollection<TransactionItem> Items { get; set; } = new List<TransactionItem>();         
     }
 }
--- src/Libraries/Core/Entities/Financial/TransactionItem.cs
+++ src/Libraries/Core/Entities/Financial/TransactionItem.cs
@@ -1,11 +1,13 @@
+using Core.Entities.Catalog;
+
 namespace Core.Entities.Financial
 {
     public class TransactionItem : BaseEntity
     {
         public string DrugUniqueCode { get; set; }
-        public Drug Drug { get; set; }
+        public virtual Drug Drug { get; set; }
         public int Quantity { get; set; }
         public decimal CustomerValue { get; set; }
-        public decimal CostPrice { get; set; }
+        public decimal CostPrice { get; set; }        
     }
 }
