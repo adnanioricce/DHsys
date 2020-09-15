4708c9e3999c7963d8519bd0078cbd5671b09de2
adnanioricce <adnanioricce@outlook.com>
2020-09-14 21:42:01 -0300

    fixing errors on CICD build scripts

--- buildApi.fsx
+++ buildApi.fsx
@@ -4,7 +4,7 @@ nuget Fake.IO.FileSystem
 nuget Fake.Core.Target
 nuget Fake.Core.String
 nuget Fake.Core.Process //"
-#load ".fake/buildapi.fsx/intellisense.fsx"
+// #load "./CI_CD/.fake/buildapi.fsx/intellisense.fsx"
 open Fake
 open Fake.Core
 open Fake.DotNet
@@ -20,48 +20,47 @@ let sourceDir = "./src/"
 //Change
 Target.initEnvironment ()
 
-Target.create "CleanSolution" (fun _ ->
-  Trace.log "--- Cleaning Solution ---"
-  !! "../src/**/bin"
-  ++ "../src/**/obj"
+Target.create "CleanApp" (fun _ ->
+  Trace.log "--- Cleaning App ---"
+  !! "src/**/bin"
+  ++ "src/**/obj"
   |> Shell.cleanDirs 
 )
 Target.create "CleanTest" (fun _ -> 
   Trace.log "--- Cleaning Tests ---"
-  !! "../tests/**/bin"
-  ++ "../tests/**/obj"
+  !! "tests/**/bin"
+  ++ "tests/**/obj"
   |> Shell.cleanDirs
 )
 Target.create "CleanBuild" (fun _ -> 
-  !! "../build/**/"
+  !! "build/**/"
   |> Shell.cleanDirs
 ) 
-Target.create "BuildLibraries" (fun _ ->  
-  Trace.log "--- Building Libraries  ---"
-  !! "../src/Libraries/**/*.*proj"  
-  -- "../src/**/*Windows*.*proj"
-  |> Seq.iter (DotNet.build id)  
-)
-Target.create "BuildTest" (fun _ ->
-  Trace.log "--- Starting Tests building ---"
-  !! "../src/tests/UnitTests/**/*.csproj"
-  -- "../src/tests/UnitTests/Desktop.Tests/Desktop.Tests.csproj"  
+Target.create "BuildApps" (fun _ -> 
+  Trace.log "--- Building Applications ---"
+  !! "src/**/Api.csproj"
   |> Seq.iter (DotNet.build id)
 )
 Target.create "RunUnitTests" (fun _ ->
   Trace.log "--- Executing Unit Tests ---"
-  !! "../tests/UnitTests/**/*.csproj"
-  -- "../tests/UnitTests/Desktop.Tests/Desktop.Tests.csproj"
+  !! "tests/UnitTests/**/*.csproj"
+  -- "src/tests/**/*Desktop*.csproj"  
   |> Seq.iter (DotNet.test id)
 )
+Target.create "PublishApi" (fun _ ->
+  Trace.log "--- Publishing Api ---"
+  !! "src/**/Api.csproj"  
+  |> Seq.iter (DotNet.publish id)
+)
+
 Target.create "All" ignore
 
 "CleanApp"
   ==> "CleanTest"
-  ==> "CleanBuild"
-  ==> "BuildLibraries"
-  ==> "BuildTest"
+  ==> "CleanBuild"  
+  ==> "BuildApps"  
   ==> "RunUnitTests"  
+  ==> "PublishApi"  
   ==> "All"  
 
 Target.runOrDefault "All"
