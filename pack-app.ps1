f8482fcbf782d43478c8931b315ad3d232343528
adnanioricce <adnanioricce@outlook.com>
2020-12-27 21:08:01 -0300

    removing unused files and editing github action

--- pack-app.ps1
+++
@@ -1,3 +0,0 @@
-$app_path = "./src/Presentation/Desktop/"
-C:\'Program Files (x86)'\'Windows Kits'\10\bin\10.0.17763.0\x86\makeappx pack /v /h SHA256 /d $app_path /p DHsys.msix
-C:\'Program Files (x86)'\'Windows Kits'\10\bin\10.0.17763.0\x86\signtool sign /fd SHA256 /a /f $(DHsysPackCert) /p $(packSignPassword) $(Build.ArtifactStagingDirectory)/DHsys.msix
