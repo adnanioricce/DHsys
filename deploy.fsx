bcf259977c64cf8ca6ef9d310b14b05790414ee7
adnanioricce <adnanioricce@outlook.com>
2022-06-02 12:24:17 -0300

    updating gitignore;updating build and deploy scripts

--- deploy.fsx
+++ deploy.fsx
@@ -49,30 +49,6 @@ Target.create "PackLibraries" (fun _ ->
   !! "src/Libraries/**/*.csproj"
   |> Seq.iter (DotNet.pack id))
 
-Target.create "PublishHelperTool" (fun _ -> 
-  Trace.log "--- Publishing Helper Tool ---"
-  let runtimes = [
-       "win-x64";       
-       "linux-x64";       
-     ]
-  let publishOptions (args:DotNet.PublishOptions) runtime =    
-    { args with
-        SelfContained = Some true
-        Framework = Some "net5.0"
-        Runtime = Some runtime
-        MSBuildParams =
-          let cliArgs (msArgs:MSBuild.CliArguments) = {
-              msArgs with
-                Properties = [("PublishSingleFile","true")]                
-                Targets = ["PublishAll"]
-          }          
-          cliArgs (DotNet.MSBuild.CliArguments.Create())        
-    }
-  let options = publishOptions (DotNet.PublishOptions.Create())
-  !! "src/Tools/Helper/*.fsproj"
-  |> Seq.iter (fun i -> runtimes |> (Seq.iter (fun r -> DotNet.publish (fun _ -> options r) i)))
-  )  
-
 Target.create "PublishWebApi" (fun _ ->
   Trace.log "--- Publishing Web Api ---"
   !! "src/Presentation/**/*.*proj"
@@ -99,7 +75,7 @@ Target.create "ZipAllBinaries" (fun _ ->
   Trace.log "--- Zipping All Binaries ---"
   let apiAssemblyName = System.Reflection.Assembly.LoadFrom(apiDir + "/Api.dll").GetName()  
   let apiZipFileName = sprintf "build/%s-%s.zip" apiAssemblyName.Name (apiAssemblyName.Version.ToString())
-  let helperZipFileName = sprintf "build/%s-%s.zip" "Helper" (apiAssemblyName.Version.ToString())  
+  // let helperZipFileName = sprintf "build/%s-%s.zip" "Helper" (apiAssemblyName.Version.ToString())  
   Zip.createZip apiDir apiZipFileName "" (int Compression.CompressionLevel.Optimal) true (Directory.GetFiles(apiDir))
   Zip.createZip toolsDir helperZipFileName "" (int Compression.CompressionLevel.Optimal) true (Directory.GetFiles(toolsDir + "/Helper"))
   )
